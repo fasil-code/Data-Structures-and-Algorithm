b2d961c542571823ce572ad297a79daf9111260c
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-09-28 23:47:28 -0700

    Create searching.cpp

---
+++ searching.cpp
@@ -0,0 +1,28 @@
+/*
+Binary search  Time complexity:O(N)  Space Complexity:O(1)
+*/
+int binarysearch(int arr[], int x, int n)
+{
+    int low = 0;
+    int high = n - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == x)
+        {
+            return mid;
+        }
+        else if (arr[mid] < x)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+
+
