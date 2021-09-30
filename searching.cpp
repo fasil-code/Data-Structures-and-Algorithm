d2ce0baef043780d9e8058be3addf5464d198fd9
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-09-30 04:19:36 -0700

    Update searching.cpp

--- searching.cpp
+++ searching.cpp
@@ -498,3 +498,50 @@ return binarysearch(vec,0,ans-1,K);
     return binarysearch(vec,ans+1,vec.size()-1,K);
     //code here
 }
+/*
+Given three arrays sorted in increasing order. Find the elements that are common in all three arrays.
+Note: can you take care of the duplicates without using any additional Data Structure?
+
+Example 1:
+
+Input:
+n1 = 6; A = {1, 5, 10, 20, 40, 80}
+n2 = 5; B = {6, 7, 20, 80, 100}
+n3 = 8; C = {3, 4, 15, 20, 30, 70, 80, 120}
+Output: 20 80
+Explanation: 20 and 80 are the only
+common elements in A, B and C.
+*/
+ vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+            int l=INT_MIN;
+            vector<int>v;
+           int i=0;
+           int j=0;
+           int k=0;
+           while(i<n1 && j<n2 && k<n3){
+               if(A[i]==B[j] && A[i]==C[k] && A[i]!=l){
+                   v.push_back(A[i]);
+                   l=A[i];
+                   i++;
+                   j++;
+                   k++;
+               }
+               else if(B[j]<C[k] || B[j]<A[i]){
+                   j++;
+               }
+               else if(A[i]<B[j] || A[i]<C[k]){
+                   i++;
+               }
+               
+               else{
+                   k++;
+               }
+           }
+           return v;
+           
+           //code here.
+        }
+
+
+
