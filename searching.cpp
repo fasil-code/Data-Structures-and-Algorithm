71802153601e948d37d51853b1e011f3e328e16c
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-09-30 10:46:39 -0700

    Update searching.cpp

--- searching.cpp
+++ searching.cpp
@@ -738,5 +738,51 @@ other numbers occur twice,
 	    
 	    
 	}
-};
+/*
 
+Sort an array according to the other 
+Medium Accuracy: 53.64% Submissions: 19672 Points: 4
+Given two integer arrays A1[ ] and A2[ ] of size N and M respectively. Sort the first array A1[ ] such that all the relative positions of the elements in the first array are the same as the elements in the second array A2[ ].
+See example for better understanding.
+Note: If elements are repeated in the second array, consider their first occurance only.
+
+Example 1:
+
+Input:
+N = 11 
+M = 4
+A1[] = {2, 1, 2, 5, 7, 1, 9, 3, 6, 8, 8}
+A2[] = {2, 1, 8, 3}
+Output: 
+2 2 1 1 8 8 3 5 6 7 9
+Explanation: Array elements of A1[] are
+sorted according to A2[]. So 2 comes first
+then 1 comes, then comes 8, then finally 3
+comes, now we append remaining elements in
+sorted order.
+*/
+
+   //Function to sort an array according to the other array.
+    vector<int> sortA1ByA2(vector<int> A1, int N, vector<int> A2, int M) 
+    { vector<int>v;
+    map<int,int>mp;
+    for(int i=0;i<N;i++){
+        mp[A1[i]]++;
+    }
+    for(int i=0;i<M;i++){
+        if(mp[A2[i]]){
+            while(mp[A2[i]]--){
+                v.push_back(A2[i]);
+            }
+            mp.erase(A2[i]);
+        }
+    }
+    for(auto x :mp){
+        int count=x.second;
+        while(count--){
+            v.push_back(x.first);
+        }
+    }
+    return v;
+        //Your code here
+    } 
