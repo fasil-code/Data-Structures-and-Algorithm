9ad0f6208c2829542128165f05229b37abfcb951
Xahoor72 <85664053+Xahoor72@users.noreply.github.com>
2021-10-01 10:14:31 +0530

    Update searching.cpp

--- searching.cpp
+++ searching.cpp
@@ -888,3 +888,49 @@ bool check(int arr[],int n,int w,long long int mid ){
         }
         return result;
     }
+ /*
+  Given two sorted arrays array1 and array2 of size m and n respectively. Find the median of the two sorted arrays.
+
+Example 1:
+
+Input:
+m = 3, n = 4
+array1[] = {1,5,9}
+array2[] = {2,3,6,7}
+Output: 5
+Explanation: The middle element for
+{1,2,3,5,6,7,9} is 5
+*/
+// your code here
+double MedianOfArrays(vector<int>& array1, vector<int>& array2)
+    {
+        // Your code goes here
+        int i=0,j=0;
+        double a=3.5;
+        vector<int>v;
+       while(i<array1.size() && j<array2.size()){
+           if(array1[i]<=array2[j]){
+               v.push_back(array1[i]);
+               i++;
+           }
+           else {
+               v.push_back(array2[j]);
+               j++;
+           }
+       }
+       
+       while(i<array1.size()){
+           v.push_back(array1[i]);
+               i++;
+       }
+        while(j<array2.size()){
+           v.push_back(array2[j]);
+               j++;
+       }
+       if(v.size()%2==1)
+       return v[v.size()/2];
+       else {
+       double ans=v[v.size()/2]+v[(v.size()/2)-1];
+           return ans/2;
+       }
+    }
