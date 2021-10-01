6279456bd05eb72ba23f25059f6bfbc2492e687b
Xahoor72 <85664053+Xahoor72@users.noreply.github.com>
2021-10-01 09:26:19 +0530

    Update searching.cpp

--- searching.cpp
+++ searching.cpp
@@ -824,3 +824,67 @@ only super prime
 	    return count;    
 	    // Your code goes here
 	}
+  /*Dilpreet wants to paint his dog's home that has n boards with different lengths.
+  The length of ith board is given by arr[i] where arr[] is an array of n integers.
+  He hired k painters for this work and each painter takes 1 unit time to paint 1 unit of the board. 
+
+The problem is to find the minimum time to get this job done if all painters start together with the constraint
+that any painter will only paint continuous boards, say boards numbered {2,3,4} or only board {1} or nothing but not boards {2,4,5}.
+
+
+Example 1:
+
+Input:
+n = 5
+k = 3
+arr[] = {5,10,30,20,15}
+Output: 35
+Explanation: The most optimal way will be:
+Painter 1 allocation : {5,10}
+Painter 2 allocation : {30}
+Painter 3 allocation : {20,15}
+Job will be done when all painters finish
+i.e. at time = max(5+10, 30, 20+15) = 35 
+*/
+
+// Your code here
+bool check(int arr[],int n,int w,long long int mid ){
+     long long  int num=1;
+      long long int total=0;
+      for(int i=0;i<n;i++){
+          if(arr[i]>mid){
+              return false;
+          }
+          total+=arr[i];
+          if(total>mid){
+              num++;
+              if(num>w){
+                  return false;
+              }
+              total=arr[i];
+          }
+      }
+      return true;
+  }
+    long long minTime(int arr[], int n, int k)
+    {
+       
+        long long sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+       long long int low=0;
+        long long int high=sum;
+        long long int result=0;
+        while(low<=high){
+            long long mid=low + (high-low)/2;
+            if(check(arr,n,k,mid)){
+                result=mid;
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
+            }
+        }
+        return result;
+    }
