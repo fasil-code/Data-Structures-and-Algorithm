5efda56748bab50ae61cbc8a7ecc603247d588a2
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-10-10 05:28:20 -0700

    Update Hashing.cpp

--- Hashing.cpp
+++ Hashing.cpp
@@ -1,1 +1,64 @@
+/*
+Check if a string is Isogram or not 
+Basic Accuracy: 55.24% Submissions: 23754 Points: 1
+Given a string S of lowercase alphabets, check if it is isogram or not. An Isogram is a string in which no letter occurs more than once.
 
+Example 1:
+
+Input:
+S = machine
+Output: 1
+Explanation: machine is an isogram
+as no letter has appeared twice. Hence
+we print 1.
+*/
+  bool isIsogram(string s)
+    {
+      int hash[256];
+        memset(hash, 0, sizeof(hash));
+      for(int i=0;i<s.size();i++){
+          hash[s[i]]++;
+      }
+      for(int i=0;i<256;i++){
+          if(hash[i]>1){
+              return false;
+          }
+      }
+      return true;
+       
+    }
+/*
+Uncommon characters 
+Basic Accuracy: 37.25% Submissions: 7863 Points: 1
+Given two strings A and B. Find the characters that are not common in the two strings. 
+
+Example 1:
+
+Input:
+A = geeksforgeeks
+B = geeksquiz
+Output: fioqruz
+Explanation: 
+The characters 'f', 'i', 'o', 'q', 'r', 'u','z' 
+are either present in A or B, but not in both.
+*/
+int hash[256]={0};
+        int hash2[256]={0};
+        for(int i=0;i<A.size();i++){
+            hash[A[i]]++;
+        }
+        for(int i=0;i<B.size();i++){
+            hash2[B[i]]++;
+        }
+        string res;
+        for(int i=0;i<256;i++){
+            if(hash[i]!=0 && hash2[i]==0){
+                res+=i;
+            }
+            else if(hash[i]==0 && hash2[i]!=0){
+                res+=i;
+            }
+        }
+         return res.size() == 0 ? "-1" : res;
+            // code here
+        }
