6f37fb8760846785d035a31a8649ecf87f020645
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-10-14 20:08:32 -0700

    Update Hashing.cpp

--- Hashing.cpp
+++ Hashing.cpp
@@ -128,3 +128,93 @@ bool subArrayExists(int arr[], int n)
         }
         return 0;
     }
+Substrings with similar first and last characters 
+Basic Accuracy: 68.4% Submissions: 1801 Points: 1
+Given string s, the task is to find the count of all substrings which have the same character at the beginning and end.
+
+Example 1:
+
+Input: s = "abcab"
+Output: 7
+Explanation: a, abca, b, bcab, 
+c, a and b
+/
+*/
+// Your code goes here
+	int countSubstringWithEqualEnds(string s)
+	{
+	   
+	    int hash[256]={0};
+	    int res=0;
+	    for(int i=0;i<s.size();i++){
+	        hash[s[i]]++;
+	    }
+	    for(int i=0;i<256;i++){
+	        res+=(hash[i]*(hash[i]+1))/2;
+	    }
+	    return res;
+	}
+/*
+String Reversal 
+Basic Accuracy: 69.01% Submissions: 1313 Points: 1
+Given a string, say S, print it in reverse manner eliminating the repeated characters and spaces.
+
+Example 1:
+
+Input: S = "GEEKS FOR GEEKS"
+Output: "SKEGROF"
+*/
+string reverseString(string s)
+{
+    //code here.
+  int hash[256]={0};
+  string str;
+  for(int i=0;i<s.size()-1;i++){
+      if(s[i]!=' ')
+      hash[s[i]]++; 
+  }
+    for(int i=s.size()-1;i>=0;i--){
+          if(hash[s[i]]>=1){
+             str+=s[i];
+             hash[s[i]]=-1;
+          }
+      }
+ 
+  return str;
+    
+}
+=======
+Subarray with 0 sum 
+Easy Accuracy: 49.91% Submissions: 71355 Points: 2
+Given an array of positive and negative numbers. Find if there is a subarray (of size at-least one) with 0 sum.
+
+Example 1:
+
+Input:
+5
+4 2 -3 1 6
+
+Output: 
+Yes
+
+Explanation: 
+2, -3, 1 is the subarray 
+with sum 0.
+*/
+//Your code here
+bool subArrayExists(int arr[], int n)
+    {
+        
+        int sum=0;
+        unordered_map<int ,int>mp;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            mp[sum]++;
+        }
+        for(auto x:mp){
+            if(x.second>1 || x.first==0){
+                return 1;
+            }
+        }
+        return 0;
+    }
