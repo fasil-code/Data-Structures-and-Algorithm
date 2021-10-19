41ff2833e21ab571d52f0ca4a1b3491cc859eb2c
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-10-19 03:33:47 -0700

    Update Linked List.cpp

--- Linked List.cpp
+++ Linked List.cpp
@@ -132,3 +132,96 @@ top=top->next;
 return x;
     // Your Code
 }
+/*
+Delete without head pointer 
+Easy Accuracy: 69.01% Submissions: 100k+ Points: 2
+You are given a pointer/ reference to the node which is to be deleted from the linked list of N nodes. The task is to delete the node. Pointer/ reference to head node is not given. 
+Note: No head reference is given to you. It is guaranteed that the node to be deleted is not a tail node in the linked list.
+
+Example 1:
+
+Input:
+N = 2
+value[] = {1,2}
+node = 1
+Output: 2
+Explanation: After deleting 1 from the
+linked list, we have remaining nodes
+as 2.
+*/
+void deleteNode(Node *del)
+    {
+            Node*temp=del->next;
+            del->data=temp->data;
+            del->next=temp->next;
+        
+       // Your code here
+    }
+/*
+Given a Matrix mat of N*N size, the task is to complete the function constructLinkedMatrix(), 
+that constructs a 2D linked list representation of the given matrix.
+
+Input : 2D matrix 
+1 2 3
+4 5 6
+7 8 9
+
+Output :
+1 -> 2 -> 3 -> NULL
+|    |    |
+v    v    v
+4 -> 5 -> 6 -> NULL
+|    |    |
+v    v    v
+7 -> 8 -> 9 -> NULL
+|    |    |
+v    v    v
+NULL NULL NULL
+*/
+Node*solve(int mat[MAX][MAX],int i,int j,int n){
+    if(i>=n || j>=n)return NULL;
+    Node*res=new Node(mat[i][j]);
+    res->right=solve(mat,i,j+1,n);
+    res->down=solve(mat,i+1,j,n);
+    return res;
+}
+Node* constructLinkedMatrix(int mat[MAX][MAX], int n)
+{
+return solve(mat,0,0,n);
+}
+//Rotate doubly Linked List 
+struct node*update(struct node*start,int p)
+{
+  node*temp=start;
+  node*hex=start;
+  for(int i=0;i<p-1;i++){
+     temp=temp->next;
+  }
+
+start=temp->next;
+start->prev=NULL;
+temp->next=NULL;
+node*q=start;
+while(q->next!=NULL){
+    q=q->next;
+}
+  hex->prev=q;
+   q->next=hex;
+   return start;
+}
+//REVERSE DOUBLY LINKED LIST
+Node* reverseDLL(Node * head)
+{
+     if(head==NULL || head->next==NULL)
+   return head;
+  Node*curr=head;
+  Node*pre=NULL;
+  Node*nex=head->next;
+  while(curr!=NULL){
+      curr->prev=curr->next;
+      curr->next=pre;
+      pre=curr;
+      curr=curr->prev;
+  }
+  return pre;
+}
