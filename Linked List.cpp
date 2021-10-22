553650b5765455fdfea211a6870e9cce1e194fb1
Xahoor72 <85664053+Xahoor72@users.noreply.github.com>
2021-10-22 16:38:35 +0530

    Update Linked List.cpp

--- Linked List.cpp
+++ Linked List.cpp
@@ -1042,3 +1042,39 @@ and end thenew list will be 4 2 3 1.
     return head;
     // Your Code here
 }
+/*
+Reverse a Linked List in groups of given size. 
+Medium Accuracy: 45.83% Submissions: 100k+ Points: 4
+Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list. 
+If the number of nodes is not a multiple of k then left-out nodes, in the end, should be considered as a group and must be reversed (See Example 2 for clarification).
+
+Example 1:
+
+Input:
+LinkedList: 1->2->2->4->5->6->7->8
+K = 4
+Output: 4 2 2 1 8 7 6 5 
+Explanation: 
+The first 4 elements 1,2,2,4 are reversed first 
+and then the next 4 elements 5,6,7,8. Hence, the 
+resultant linked list is 4->2->2->1->8->7->6->5.
+*/
+      // Your Code
+      struct node *reverse (struct node *head, int k)
+    { 
+        // Complete this method
+        struct node*p=head,*q=NULL,*r=NULL;
+        int count=0;
+        while(p && count<k){
+            r=q;
+            q=p;
+            p=p->next;
+            q->next=r;
+            count++;
+        }
+        //struct node*next=p;
+        if(p){
+        head->next=reverse(p,k);
+        }
+        return q;
+    }
