51b40e148c305f1bffa97ecdeb6c723ed89612c7
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-10-23 06:00:13 -0700

    Update STACK.cpp

--- STACK.cpp
+++ STACK.cpp
@@ -45,3 +45,71 @@ queue<int> rev(queue<int> q)
     }
     return q;
 }
+/*
+Evaluation of Postfix Expression 
+Easy Accuracy: 53.28% Submissions: 24951 Points: 2
+Given string S representing a postfix expression, the task is to evaluate the expression and find the final value. 
+Operators will only include the basic arithmetic operators like *, /, + and -.
+Example 1:
+Input: S = "231*+9-"
+Output: -4
+Explanation:
+After solving the given expression, 
+we have -4 as result.
+*/
+int evaluatePostfix(string S)
+    {
+        stack<int>s;
+        for(int i=0;i<S.size();i++){
+            if(S[i]>='0' && S[i]<='9' ){
+                s.push(S[i]-48);
+            }
+            else{
+                int res=0;
+                int x=s.top();
+                s.pop();
+                int y=s.top();
+                s.pop();
+                if(S[i]=='*'){
+                    res=x*y;
+                }
+                else if(S[i]=='+'){
+                    res=x+y;
+                }
+              else if(S[i]=='/'){
+                    res=y/x;
+                }  else if(S[i]=='-'){
+                    res=y-x;
+                }
+            s.push(res);
+            }
+        }
+        return s.top();    
+    }
+//sort a stack
+void insert(stack<int>&s,int temp){
+    if(s.empty()|| temp>=s.top()){
+         s.push(temp);
+    }
+    else{
+        int ptr=s.top();
+        s.pop();
+        insert(s,temp);
+        s.push(ptr);
+    }
+    
+}
+void SortedStack :: sort()
+{
+    if(s.empty()){
+        return;
+    }
+    else{
+        int temp=s.top();
+        s.pop();
+        sort();
+        insert(s,temp);
+    }
+ 
+}
+
