7a6722f85e31e5d1268b7e1e47453a1bcc4cbd5e
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-10-25 00:15:06 -0700

    Update STACK.cpp

--- STACK.cpp
+++ STACK.cpp
@@ -608,3 +608,44 @@ if(i == ans.size()) ans = "0";
 else ans =ans.substr(i);
 return ans;
     }
+/*
+Maximum Rectangular Area in a Histogram 
+Medium Accuracy: 47.42% Submissions: 44035 Points: 4
+Find the largest rectangular area possible in a given histogram where the largest rectangle can be made of a number of contiguous bars.
+For simplicity, assume that all bars have the same width and the width is 1 unit, there will be N bars height of each bar will be given by the array arr.
+
+Example 1:
+
+Input:
+N = 7
+arr[] = {6,2,5,4,5,1,6}
+Output: 12
+Explanation: 
+*/
+long long getMaxArea(long long arr[], int n)
+    {   stack<int> st;
+        long area=0,max=0; 
+        int i=0;
+while(i<n){
+    if(st.empty()||arr[i]>=arr[st.top()]){
+st.push(i);
+i++;
+}
+else{
+int curr = st.top();
+st.pop();
+area = arr[curr]*(st.empty()?i:(i-1-st.top()));
+if(area>max)
+max=area;
+}
+}
+while(!st.empty()){
+int curr = st.top();
+st.pop();
+area = arr[curr]*(st.empty()?i:(i-1-st.top()));
+if(area>max)
+max=area;
+}
+return max;      
+    }
+
