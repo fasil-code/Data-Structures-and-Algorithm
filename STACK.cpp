796e8f10559bdbc16f5883a9063e2d3a0e38b35c
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-10-25 02:57:15 -0700

    Update STACK.cpp

--- STACK.cpp
+++ STACK.cpp
@@ -648,4 +648,41 @@ max=area;
 }
 return max;      
     }
+/*
+Longest valid Parentheses 
+Hard Accuracy: 48.35% Submissions: 12955 Points: 8
+Given a string S consisting of opening and closing parenthesis '(' and ')'. Find length of the longest valid parenthesis substring.
 
+A parenthesis string is valid if:
+
+For every opening parenthesis, there is a closing parenthesis.
+Opening parenthesis must be closed in the correct order.
+Example 1:
+
+Input: S = ((()
+Output: 2
+Explaination: The longest valid 
+parenthesis substring is "()".
+*/
+int maxLength(string S){
+        stack<char>s;
+        int n=S.size();
+        int result=-1;
+        for(int i=0;i<n;i++){
+            if(S[i]=='('){
+                s.push(i);
+                
+            }
+            else if(S[i]=')'){
+        if(!s.empty()){
+            s.pop();
+        }
+            
+            if (!s.empty())
+				result = max(result, i - s.top());
+}
+			else
+				s.push(i);
+	}
+	return result;      
+    }
