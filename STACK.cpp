452e30e80e3a4ec2ec8d1f8dc3cb33516b444437
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-10-25 22:41:17 -0700

    Update STACK.cpp

--- STACK.cpp
+++ STACK.cpp
@@ -733,3 +733,53 @@ st.push(arr[i]);
 }
 return false;	    
 	}
+/*
+Reverse First K elements of Queue 
+Easy Accuracy: 57.92% Submissions: 36942 Points: 2
+Given an integer K and a queue of integers, we need to reverse the order of the first K elements of the queue, leaving the other elements in the same relative order.
+
+Only following standard operations are allowed on queue.
+
+enqueue(x) : Add an item x to rear of queue
+dequeue() : Remove an item from front of queue
+size() : Returns number of elements in queue.
+front() : Finds front item.
+Example 1:
+
+Input:
+5 3
+1 2 3 4 5
+
+Output: 
+3 2 1 4 5
+
+Explanation: 
+After reversing the given
+input from the 3rd position the resultant
+output will be 3 2 1 4 5.
+
+*/
+queue<int> modifyQueue(queue<int> q, int k) {
+
+stack<int>s;
+while(k>0){
+    s.push(q.front());
+    q.pop();
+    k--;
+}
+queue<int>q1;
+while(!q.empty()){
+    q1.push(q.front());
+    q.pop();
+}
+while(!s.empty()){
+    q.push(s.top());
+    s.pop();
+}
+while(!q1.empty()){
+    q.push(q1.front());
+    q1.pop();
+}
+return q;
+    // add code here.
+}
