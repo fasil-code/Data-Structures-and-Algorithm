642c4b284e7e4ad70461242b425912c7e01cfa60
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-10-27 07:17:40 -0700

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -178,3 +178,53 @@ First negative integer for each window of size k
            }   
        }
          return v; }    
+    
+    /*
+    Maximum of all subarrays of size k 
+Medium Accuracy: 44.42% Submissions: 50920 Points: 4
+Given an array arr[] of size N and an integer K. Find the maximum for each and every contiguous subarray of size K.
+
+Example 1:
+
+Input:
+N = 9, K = 3
+arr[] = 1 2 3 1 4 5 2 3 6
+Output: 
+3 3 4 5 5 5 6 
+Explanation: 
+1st contiguous subarray = {1 2 3} Max = 3
+2nd contiguous subarray = {2 3 1} Max = 3
+3rd contiguous subarray = {3 1 4} Max = 4
+4th contiguous subarray = {1 4 5} Max = 5
+5th contiguous subarray = {4 5 2} Max = 5
+6th contiguous subarray = {5 2 3} Max = 5
+7th contiguous subarray = {2 3 6} Max = 6
+    */
+     vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        deque<int> dq;
+        vector<int> res;
+        int i = 0, j = 0;
+        while(j < n)
+        {
+            while(!dq.empty() && dq.back() < arr[j])
+                dq.pop_back();
+                
+            dq.push_back(arr[j]);
+            
+            if(j - i + 1 == k)
+            {
+            res.push_back(dq.front());
+                if(arr[i] == dq.front())
+                {
+                    dq.pop_front();
+                }
+                i++;
+                j++;
+            }
+            else j++;
+        }
+        return res;
+        
+      
+    }
