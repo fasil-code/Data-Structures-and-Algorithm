e41d52fb32cbe88da05fa33cf45fe405f45eb743
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-10-27 06:22:30 -0700

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -126,3 +126,55 @@ while(q.size()!=1){
 return ans;
        
     }
+    /*
+    First negative integer in every window of size k 
+Easy Accuracy: 54.27% Submissions: 15511 Points: 2
+Given an array A[] of size N and a positive integer K, find the first negative integer for each and every window(contiguous subarray) of size K.
+
+ 
+
+Example 1:
+
+Input : 
+N = 5
+A[] = {-8, 2, 3, -6, 10}
+K = 2
+Output : 
+-8 0 -6 -6
+Explanation :
+First negative integer for each window of size k
+{-8, 2} = -8
+{2, 3} = 0 (does not contain a negative integer)
+{3, -6} = -6
+{-6, 10} = -6
+    */
+    vector<long long> printFirstNegativeInteger(long long int A[],
+                                             long long int N, long long int k) {
+        
+        vector<long long>v;
+        queue<long long >q;
+       long long int i=0,j=0;
+       while(j<N){
+           if(A[j]<0){
+               q.push(A[j]);
+           }
+           if(j-i+1<k){
+               j++;
+           }
+           else if(j-i+1==k){
+               if(q.empty()){
+                   v.push_back(0);
+               }
+               else{
+                   v.push_back(q.front()); 
+                   
+               }
+               
+               if(!q.empty() && A[i]<0){
+                   q.pop();
+               }
+               i++;
+               j++;
+           }   
+       }
+         return v; }    
