12f6323c722ff8e6bb58b603c289bd8b077e1684
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-10-28 23:11:44 -0700

    Update TREE.cpp

--- TREE.cpp
+++ TREE.cpp
@@ -45,3 +45,218 @@ struct Node{
     struct Node*left;
     struct Node*right;
 }
+*/
+//Count Leaves in Binary Tree
+int countLeaves(Node* root)
+{
+  if(root==NULL)return 0;
+  if(root->left==NULL && root->right==NULL)return 1;
+  else{
+      return countLeaves(root->left)+countLeaves(root->right);
+  }
+}
+//Minimum element in BST
+int minValue(Node* root) {
+    
+    if(root==NULL)return -1;
+    if(root->left==NULL)return root->data;
+    else{
+      return minValue(root->left) ; 
+    }
+}
+//Size of Binary Tree 
+int getSize(Node* node)
+{
+   if(node==NULL)return 0;
+   else{
+       return 1+getSize(node->left)+getSize(node->right);
+   }
+}
+//INOTRDER TRAVERSAL
+  void solve(Node*root,vector<int>&v){
+        if(root==NULL)return;
+        else{
+            solve(root->left,v);
+            v.push_back(root->data);
+            solve(root->right,v);
+        }
+    }
+    vector<int> inOrder(Node* root) {
+        vector<int>v;
+        solve(root,v);
+        return v;   
+    }
+//POSTORDER TRAVERSAL
+void fun(Node*root,vector<int>&v){
+    if(root!=NULL){
+        fun(root->left,v);
+        fun(root->right,v);
+        v.push_back(root->data);
+    }
+}
+vector <int> postOrder(Node* root)
+{
+    vector<int>v;
+    fun(root,v);
+    return v;
+  // Your code here
+}
+//preorder
+void fun(Node*root,vector<int>&v){
+    if(root!=NULL){
+          v.push_back(root->data);
+    fun(root->left,v);
+    fun(root->right,v);
+    }
+  
+}
+
+//Function to return a list containing the preorder traversal of the tree.
+vector <int> preorder(Node* root)
+{
+    vector<int>v;
+    fun(root,v);
+    return v;
+}
+// MIRROR TREE Given a Binary Tree, convert it into its mirror.
+ void mirror(Node* node) {
+        if(node==NULL)return;
+        mirror(node->left);
+        mirror(node->right);
+        swap(node->left,node->right);
+    }
+/*
+Expression Tree 
+Easy Accuracy: 55.88% Submissions: 24149 Points: 2
+Given a full binary expression tree consisting of basic binary operators (+ , – ,*, /) and some integers, Your task is to evaluate the expression tree.
+
+Example 1:
+
+Input: 
+              +
+           /     \
+          *       -
+        /  \    /   \
+       5    4  100  20 
+
+Output: 100
+
+Explanation:
+((5 * 4) + (100 - 20)) = 100
+*/
+int evalTree(node* root) {
+  //  if(root==NULL)return 0;
+     if(!root->left and !root->right) return  stoi(root->data);
+        
+    
+        int l=evalTree(root->left);
+        int r=evalTree(root->right);
+        if(root->data=="+"){
+            return l+r;
+        }
+        else if(root->data=="*"){
+            return l*r;
+        }
+        else if(root->data=="-"){
+            return l-r;
+        }
+        else{
+            return (l/r);
+        }    // Your code here
+    }
+/*
+K distance from root 
+Easy Accuracy: 55.52% Submissions: 53384 Points: 2
+Given a Binary Tree of size N and an integer K. Print all nodes that are at distance k from root (root is considered at distance 0 from itself). Nodes should be printed from left to right. If k is more that height of tree, nothing should be printed.
+
+For example, if below is given tree and k is 2. Output should be 4 5 6.
+
+          1
+       /     \
+     2        3
+   /         /   \
+  4        5    6 
+     \
+      8
+
+Example 1:
+
+Input:
+K = 0
+      1
+    /   \
+   3     2
+Output: 1
+
+*/
+void solve(Node*root,vector<int>&v,int k){
+    if(root==NULL)return;
+    if(k==0){
+        v.push_back(root->data);
+    }
+    else{
+        solve(root->left,v,k-1);
+        solve(root->right,v,k-1);
+    }
+}
+vector<int> Kdistance(struct Node *root, int k)
+{
+    vector<int>v;
+solve(root,v,k);
+return v;
+}
+/*
+Odd even level difference 
+Easy Accuracy: 53.67% Submissions: 22445 Points: 2
+Given a Binary Tree. Find the difference between the sum of node values at even levels and the sum of node values at the odd levels.
+
+Example 1:
+
+Input:
+            1
+          /   \
+         2     3
+
+Output: -4
+
+Explanation:
+sum at odd levels - sum at even levels
+= (1)-(2+3) = 1-5 = -4
+*/
+ int getLevelDiff(Node *root)
+    {
+      if(root==NULL)return 0;
+      int sum=0;
+      int lvl=1;
+      int oddsum=0;
+      int evensum=0;
+      queue<Node*>q;
+      q.push(root);
+      while(!q.empty()){
+          int size=q.size();
+          while(size--){
+              Node*temp=q.front();
+              sum+=temp->data;
+              q.pop();
+              if(temp->left){
+                  q.push(temp->left);
+              }
+              if(temp->right){
+                  q.push(temp->right);
+              }
+          }
+      
+          if(lvl%2==1){
+              oddsum+=sum;
+              
+          }
+          else{
+              evensum+=sum;
+          }
+        lvl++;
+          sum=0;
+          
+      }
+      return oddsum-evensum;
+      
+    }
