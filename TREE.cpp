eb9c9f5921c51a9e097d1534c298ee076cb43efc
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-10-29 09:58:08 -0700

    Update TREE.cpp

--- TREE.cpp
+++ TREE.cpp
@@ -509,3 +509,19 @@ vector <int> diagonalSum(Node* root) {
     return v;
    
 }
+//Symmetric Tree
+  bool solve(Node*r1,Node*r2){
+        if(!r1 && !r2)return true;
+          if(!r1&& r2)return false;
+          if(r1&& !r2)return false;
+            if(r1->data!= r2->data)return false;
+            else{
+                return solve(r1->left,r2->right)&& solve(r1->right,r2->left);
+            }
+    }
+    // return true/false denoting whether the tree is Symmetric or not
+    bool isSymmetric(struct Node* root)
+    {
+       return solve(root,root);
+	    // Code here
+    }
