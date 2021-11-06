a647f65b9ded4b3513bf1abb436ce9577a329a7f
fasil-code <77100387+fasil-code@users.noreply.github.com>
2021-11-06 05:42:18 -0700

    Update TREE2.cpp

--- TREE2.cpp
+++ TREE2.cpp
@@ -713,9 +713,11 @@ return deSerializeUtil(A,pointer);
 Medium Accuracy: 26.78% Submissions: 100k+ Points: 4
 Given a Binary Tree, find its Boundary Traversal. The traversal should be in the following order: 
 
-Left boundary nodes: defined as the path from the root to the left-most node ie- the leaf node you could reach when you always travel preferring the left subtree over the right subtree. 
+Left boundary nodes: defined as the path from the root to the left-most node ie- the leaf node you could reach when you always travel preferring the left subtree over
+the right subtree. 
 Leaf nodes: All the leaf nodes except for the ones that are part of left or right boundary.
-Reverse right boundary nodes: defined as the path from the right-most node to the root. The right-most node is the leaf node you could reach when you always travel preferring the right subtree over the left subtree. Exclude the root from this as it was already included in the traversal of left boundary nodes.
+Reverse right boundary nodes: defined as the path from the right-most node to the root. The right-most node is the leaf node you could reach when you always travel
+preferring the right subtree over the left subtree. Exclude the root from this as it was already included in the traversal of left boundary nodes.
 Note: If the root doesn't have a left subtree or right subtree, then the root itself is the left or right boundary. 
 
 Example 1:
@@ -731,8 +733,7 @@ Input:
    
 Output: 1 2 4 8 9 6 7 3
 Explanation:
-   
-   
+     
    */
             void leaf(Node*root,vector<int>&v){
     if(root==NULL){
@@ -1209,7 +1210,8 @@ It takes 7s to burn the complete tree.
       /*
       Nodes at given distance in binary tree 
 Hard Accuracy: 45.23% Submissions: 23906 Points: 8
-Given a binary tree, a target node in the binary tree, and an integer value k, find all the nodes that are at distance k from the given target node. No parent pointers are available.
+Given a binary tree, a target node in the binary tree, and an integer value k, find all the nodes that are at distance k from the given target node.
+No parent pointers are available.
 
 Example 1:
 
