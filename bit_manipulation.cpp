e96fa4d9ba9ba7673ab153ce0d0e80123aef6de7
Xahoor72 <85664053+Xahoor72@users.noreply.github.com>
2021-11-13 15:22:20 +0530

    Update bit_manipulation.cpp

--- bit_manipulation.cpp
+++ bit_manipulation.cpp
@@ -124,3 +124,28 @@ int sum(int a , int b)
        }
        return a;
    }
+/*
+Longest Consecutive 1's 
+Easy Accuracy: 57.61% Submissions: 20627 Points: 2
+Given a number N. Find the length of the longest consecutive 1s in its binary representation.
+
+Example 1:
+
+Input: N = 14
+Output: 3
+Explanation: 
+Binary representation of 14 is 
+1110, in which 111 is the longest 
+consecutive set bits of length is 3.
+*/
+    int maxConsecutiveOnes(int N)
+    {
+        // code here]
+        int c=0;
+        while(N){
+            N=N&(N<<1);
+            c++;
+        }
+        return c;
+    }
+
