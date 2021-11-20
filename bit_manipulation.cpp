63c0b68e86c25c232c141aa4c7a0f88f85e8e13f
Xahoor72 <85664053+Xahoor72@users.noreply.github.com>
2021-11-20 11:30:49 +0530

    Update bit_manipulation.cpp

--- bit_manipulation.cpp
+++ bit_manipulation.cpp
@@ -688,3 +688,58 @@ element 1 occurs thrice.
         return ans
     }
 };
+/*
+Maximum XOR of two numbers in an array 
+Medium Accuracy: 81.5% Submissions: 3155 Points: 4
+Given an array of non-negative integers of size N. Find the maximum possible XOR between two numbers present in the array.
+
+Example 1:
+
+â€‹Input:
+Arr = {25, 10, 2, 8, 5, 3}
+Output: 28
+Explanation:
+The maximum result is 5 ^ 25 = 28.
+*/
+ int max_xor(int arr[] , int n)
+    {
+        //code here
+        int maxx = 0, mask = 0;
+ 
+    set<int> se;
+ 
+    for (int i = 30; i >= 0; i--) {
+ 
+        // set the i'th bit in mask
+        // like 100000, 110000, 111000..
+        mask |= (1 << i);
+ 
+        for (int i = 0; i < n; ++i) {
+ 
+            // Just keep the prefix till
+            // i'th bit neglecting all
+            // the bit's after i'th bit
+            se.insert(arr[i] & mask);
+        }
+ 
+        int newMaxx = maxx | (1 << i);
+ 
+        for (int prefix : se) {
+ 
+            // find two pair in set
+            // such that a^b = newMaxx
+            // which is the highest
+            // possible bit can be obtained
+            if (se.count(newMaxx ^ prefix)) {
+                maxx = newMaxx;
+                break;
+            }
+        }
+ 
+        // clear the set for next
+        // iteration
+        se.clear();
+    }
+ 
+    return maxx;
+    }
